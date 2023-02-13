228cebf1f0e4fd5bc18fa8b74eb899c6bbbb7a28
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:45 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript Audio Spectrogram 2D & 3D Chart
 
-![JavaScript Audio Spectrogram 2D & 3D Chart](surfaceScrollingGrid.png)
+![JavaScript Audio Spectrogram 2D & 3D Chart](surfaceScrollingGrid-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,13 +8,13 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
@@ -23,7 +23,7 @@ This example showcases the `SurfaceScrollingGridSeries` feature of LightningChar
 
 This works otherwise same as `SurfaceGridSeries`, except that it is designed explicitly for applications that are based on pushing new samples in, while shifting old samples out. For this reason, its data modification API is different.
 
-`SurfaceScrollingGridSeries` is the 3D variant of [`HeatmapScrollingGridSeries`](https://www.arction.com/lightningchart-js-api-documentation/v3.2.0/classes/heatmapscrollinggridseries.html). This is highlighted in this example, by displaying the same data in both 2D and 3D at the same time on top of another.
+`SurfaceScrollingGridSeries` is the 3D variant of [`HeatmapScrollingGridSeries`](https://lightningchart.com/lightningchart-js-api-documentation/v3.2.0/classes/heatmapscrollinggridseries.html). This is highlighted in this example, by displaying the same data in both 2D and 3D at the same time on top of another.
 
 This example visualizes the frequency spectrum of a short audio clip (about 9 seconds long) of a truck driving. The visualization is done in real-time, simulating applications that measure, analyse and display audio metrics in real-time. One such application is the [LightningChart Audio analysis showcase](https://arction.github.io/lcjs-showcase-audio/).
 
@@ -39,13 +39,13 @@ The performance bottleneck in scrolling spectrogram applications is generally ax
 
 # More Surface Examples
 
-- [Static Surface Grid Chart](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0912-surfaceGrid.html) | This example showcases the most simple usage of surface grid series, and also has a bit more detailed description of the related terms.
+-   [Static Surface Grid Chart](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0912-surfaceGrid.html) | This example showcases the most simple usage of surface grid series, and also has a bit more detailed description of the related terms.
 
-- [Chunked Surface Grid Chart](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0916-surfaceChunkLoad.html) | This example shows how surface grid series can be split into sub sections which are loaded individually for better performance and response time.
+-   [Chunked Surface Grid Chart](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0916-surfaceChunkLoad.html) | This example shows how surface grid series can be split into sub sections which are loaded individually for better performance and response time.
 
-- [Intensity Surface Grid Chart](https://www.arction.com/lightningchart-js-interactive-examples/examples/lcjs-example-0914-surfaceIntensityGrid.html) | This example showcases how surface grid series can be colored based on a different data set than the height map used in this example.
+-   [Intensity Surface Grid Chart](https://lightningchart.com/lightningchart-js-interactive-examples/examples/lcjs-example-0914-surfaceIntensityGrid.html) | This example showcases how surface grid series can be colored based on a different data set than the height map used in this example.
 
-- [All Surface Examples](https://www.arction.com/lightningchart-js-interactive-examples/search.html?t=surface)
+-   [All Surface Examples](https://lightningchart.com/lightningchart-js-interactive-examples/search.html?t=surface)
 
 
 ## API Links
@@ -67,32 +67,32 @@ The performance bottleneck in scrolling spectrogram applications is generally ax
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chart3d.html
-[3D axis]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/axis3d.html
-[Paletted Fill]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/palettedfill.html
-[Color lookup table]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lut.html
-[Color HSV factory]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorhsv
-[Color shading styles (3D)]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#colorshadingstyles
-[Surface Grid Series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/newsurfacegridseries3d.html
-[Heatmap Grid Series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/heatmapgridseriesintensityvalues.html
-[Legend Box]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chartxy.html#addlegendbox
-[Axis scroll strategies]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#axisscrollstrategies
-[Axis tick strategies]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/globals.html#axistickstrategies
+[3D chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart3D.html
+[3D axis]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Axis3D.html
+[Paletted Fill]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PalettedFill.html
+[Color lookup table]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LUT.html
+[Color HSV factory]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/functions/ColorHSV.html
+[Color shading styles (3D)]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/ColorShadingStyles.html
+[Surface Grid Series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/SurfaceGridSeries3D.html
+[Heatmap Grid Series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/HeatmapGridSeriesIntensityValues.html
+[Legend Box]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart.html#addLegendBox
+[Axis scroll strategies]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/AxisScrollStrategies.html
+[Axis tick strategies]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/variables/AxisTickStrategies.html
 
--- src/index.js
+++ src/index.js
@@ -2,218 +2,138 @@
  * Example showcasing the Surface Scrolling Grid Series feature of LightningChart JS.
  */
 
-const lcjs = require("@arction/lcjs");
-const {
-  lightningChart,
-  LUT,
-  ColorHSV,
-  PalettedFill,
-  emptyLine,
-  AxisScrollStrategies,
-  AxisTickStrategies,
-  Themes,
-} = lcjs;
+const lcjs = require('@arction/lcjs')
+const { lightningChart, LUT, ColorHSV, PalettedFill, emptyLine, AxisScrollStrategies, AxisTickStrategies, regularColorSteps, Themes } = lcjs
 
-const historyMs = 27 * 1000;
+const historyMs = 27 * 1000
 // Sampling rate as samples per second.
 const sampleRateHz = 35
 const sampleIntervalMs = 1000 / sampleRateHz
 
 // Create empty dashboard and charts.
 const dashboard = lightningChart()
-  .Dashboard({
-    numberOfColumns: 2,
-    numberOfRows: 2,
-    // theme: Themes.darkGold
-  })
-  .setRowHeight(0, 1)
-  .setRowHeight(1, 2);
+    .Dashboard({
+        numberOfColumns: 2,
+        numberOfRows: 2,
+        // theme: Themes.darkGold
+    })
+    .setRowHeight(0, 1)
+    .setRowHeight(1, 2)
 
-let labelLoading = dashboard.addUIElement().setText("Loading example data ...");
+let labelLoading = dashboard.addUIElement().setText('Loading example data ...')
 
 // Load example data from file.
-fetch(
-  document.head.baseURI +
-    "examples/assets/0913/audio2ch.json"
-)
-  .then((r) => r.json())
-  .then((data) => {
-    labelLoading.dispose();
-    labelLoading = undefined;
+fetch(document.head.baseURI + 'examples/assets/0913/audio2ch.json')
+    .then((r) => r.json())
+    .then((data) => {
+        labelLoading.dispose()
+        labelLoading = undefined
 
-    // Define value -> color lookup table.
-    const lut = new LUT({
-      steps: [
-        {
-          value: 0,
-          color: ColorHSV(0, 1, 0),
-          label: `-100`,
-        },
-        {
-          value: 255 * (1 / 6),
-          color: ColorHSV(270, 0.84, 0.2),
-          label: `-88`,
-        },
-        {
-          value: 255 * (2 / 6),
-          color: ColorHSV(289, 0.86, 0.35),
-          label: `-77`,
-        },
-        {
-          value: 255 * (3 / 6),
-          color: ColorHSV(324, 0.97, 0.56),
-          label: `-65`,
-        },
-        {
-          value: 255 * (4 / 6),
-          color: ColorHSV(1, 1, 1),
-          label: `-53`,
-        },
-        {
-          value: 255 * (5 / 6),
-          color: ColorHSV(44, 0.64, 1),
-          label: `-42`,
-        },
-        {
-          value: 255,
-          color: ColorHSV(62, 0.32, 1),
-          label: `-30`,
-        },
-      ],
-      units: "dB",
-      interpolate: true,
-    });
-
-    const rowStep = 40;
-    const intensityValueToDb = (value) => -100 + (value / 255) * (-30 - -100);
-
-    let channelList = [
-      {
-        name: "Channel 1",
-        data: data.ch1,
-        columnIndex: 0,
-      },
-      {
-        name: "Channel 2",
-        data: data.ch2,
-        columnIndex: 1,
-      },
-    ];
-
-    channelList = channelList.map((channel) => {
-      const rows = channel.data[0].length;
-      const chart2D = dashboard
-        .createChartXY({
-          columnIndex: channel.columnIndex,
-          rowIndex: 0,
+        // Define value -> color lookup table.
+        const theme = dashboard.getTheme()
+        const lut = new LUT({
+            steps: regularColorSteps(0, 255, theme.examples.spectrogramColorPalette),
+            units: 'dB',
+            interpolate: true,
         })
-        .setTitle(`${channel.name} | 2D audio spectrogram`);
-      chart2D
-        .getDefaultAxisX()
-        .setTickStrategy(AxisTickStrategies.Time)
-        .setScrollStrategy(AxisScrollStrategies.progressive)
-        .setInterval(-historyMs, 0);
-      chart2D.getDefaultAxisY().setTitle("Frequency (Hz)");
 
-      const chart3D = dashboard
-        .createChart3D({
-          columnIndex: channel.columnIndex,
-          rowIndex: 1,
+        const rowStep = 40
+        const intensityValueToDb = (value) => -100 + (value / 255) * (-30 - -100)
+
+        let channelList = [
+            {
+                name: 'Channel 1',
+                data: data.ch1,
+                columnIndex: 0,
+            },
+            {
+                name: 'Channel 2',
+                data: data.ch2,
+                columnIndex: 1,
+            },
+        ]
+
+        channelList = channelList.map((channel) => {
+            const rows = channel.data[0].length
+            const chart2D = dashboard
+                .createChartXY({
+                    columnIndex: channel.columnIndex,
+                    rowIndex: 0,
+                })
+                .setTitle(`${channel.name} | 2D audio spectrogram`)
+            chart2D
+                .getDefaultAxisX()
+                .setTickStrategy(AxisTickStrategies.Time)
+                .setScrollStrategy(AxisScrollStrategies.progressive)
+                .setInterval({ start: -historyMs, end: 0, stopAxisAfter: false })
+            chart2D.getDefaultAxisY().setTitle('Frequency (Hz)')
+
+            const chart3D = dashboard
+                .createChart3D({
+                    columnIndex: channel.columnIndex,
+                    rowIndex: 1,
+                })
+                .setTitle(`${channel.name} | 3D audio spectrogram`)
+
+            chart3D
+                .getDefaultAxisX()
+                .setTickStrategy(AxisTickStrategies.Time)
+                .setScrollStrategy(AxisScrollStrategies.progressive)
+                .setInterval({ start: -historyMs, end: 0, stopAxisAfter: false })
+            chart3D
+                .getDefaultAxisY()
+                .setTitle('Intensity (dB)')
+                .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
+                    ticks.setFormattingFunction((y) => intensityValueToDb(y).toFixed(0)),
+                )
+            chart3D.getDefaultAxisZ().setTitle('Frequency (Hz)')
+
+            const heatmapSeries2D = chart2D
+                .addHeatmapScrollingGridSeries({
+                    scrollDimension: 'columns',
+                    resolution: rows,
+                    step: { x: sampleIntervalMs, y: rowStep },
+                })
+                .setFillStyle(new PalettedFill({ lut }))
+                .setWireframeStyle(emptyLine)
+                .setDataCleaning({ maxDataPointCount: 10000 })
+
+            const surfaceSeries3D = chart3D
+                .addSurfaceScrollingGridSeries({
+                    scrollDimension: 'columns',
+                    columns: Math.ceil(historyMs / sampleIntervalMs),
+                    rows,
+                    step: { x: sampleIntervalMs, z: rowStep },
+                })
+                .setFillStyle(new PalettedFill({ lut, lookUpProperty: 'y' }))
+                .setWireframeStyle(emptyLine)
+
+            return { ...channel, chart2D, chart3D, heatmapSeries2D, surfaceSeries3D }
         })
-        .setTitle(`${channel.name} | 3D audio spectrogram`)
 
-      chart3D
-        .getDefaultAxisX()
-        .setTickStrategy(AxisTickStrategies.Time)
-        .setScrollStrategy(AxisScrollStrategies.progressive)
-        .setInterval(-historyMs, 0);
-      chart3D
-        .getDefaultAxisY()
-        .setTitle("Intensity (Db)")
-        .setTickStrategy(AxisTickStrategies.Numeric, (ticks) =>
-          ticks.setFormattingFunction((y) => intensityValueToDb(y).toFixed(0))
-        );
-      chart3D.getDefaultAxisZ().setTitle("Frequency (Hz)");
-
-      const heatmapSeries2D = chart2D
-        .addHeatmapScrollingGridSeries({
-          scrollDimension: "columns",
-          resolution: rows,
-          step: { x: sampleIntervalMs, y: rowStep },
-        })
-        .setFillStyle(new PalettedFill({ lut }))
-        .setWireframeStyle(emptyLine)
-        .setDataCleaning({ maxDataPointCount: 10000 });
-
-      const surfaceSeries3D = chart3D
-        .addSurfaceScrollingGridSeries({
-          scrollDimension: "columns",
-          columns: Math.ceil(historyMs / sampleIntervalMs),
-          rows,
-          step: { x: sampleIntervalMs, z: rowStep },
-        })
-        .setFillStyle(new PalettedFill({ lut, lookUpProperty: "y" }))
-        .setWireframeStyle(emptyLine);
-
-      return { ...channel, chart2D, chart3D, heatmapSeries2D, surfaceSeries3D };
-    });
-
-    // Setup infinite streaming from static data set.
-    let iSample = 0;
-    setInterval(() => {
-      // Push 1 new sample to all channels and series.
-      const samples = channelList.map(channel => channel.data[iSample % channel.data.length])
-      iSample += 1
-      bufferIncomingSamples(samples, (appendSamples) => {
-        channelList.forEach((channel, i) => {
-          channel.heatmapSeries2D.addIntensityValues([appendSamples[i]]);
-          channel.surfaceSeries3D.addValues({ yValues: [appendSamples[i]] });
-        });
-      })
-    }, sampleIntervalMs)
-
-    // The following logic ensures a static sampling rate, even if input data might vary.
-    // This is done by skipping too frequent samples and duplicating too far apart samples.
-    // The precision can be configured by simply changing value of `sampleRateHz`
-    let lastSample
-    let tFirstSample = 0
-    const bufferIncomingSamples = (sample, clbk) => {
-      const tNow = performance.now()
-      if (lastSample === undefined) {
-          clbk(sample)
-          lastSample = { sample, time: tNow, i: 0 }
-          tFirstSample = tNow
-          return
-      }
-
-      let nextSampleIndex = lastSample.i + 1
-      let nextSampleTimeExact = tFirstSample + nextSampleIndex * sampleIntervalMs
-      let nextSampleTimeRangeMin = nextSampleTimeExact - sampleIntervalMs / 2
-      let nextSampleTimeRangeMax = nextSampleTimeExact + sampleIntervalMs / 2
-      if (tNow < nextSampleTimeRangeMin) {
-          // Too frequent samples must be scrapped. If this results in visual problems then sample rate must be increased.
-          // console.warn(`Skipped too frequent sample`)
-          return
-      }
-      if (tNow > nextSampleTimeRangeMax) {
-          // At least 1 sample was skipped. In this case, the missing sample slots are filled with the values of the last sample.
-          let repeatedSamplesCount = 0
-          do {
-              clbk(lastSample.sample)
-              repeatedSamplesCount += 1
-              nextSampleIndex += 1
-              nextSampleTimeExact = tFirstSample + nextSampleIndex * sampleIntervalMs
-              nextSampleTimeRangeMin = nextSampleTimeExact - sampleIntervalMs / 2
-              nextSampleTimeRangeMax = nextSampleTimeExact + sampleIntervalMs / 2
-          } while (tNow > nextSampleTimeRangeMax)
-
-          clbk(sample)
-          lastSample = { sample, time: tNow, i: nextSampleIndex }
-          // console.warn(`Filled ${repeatedSamplesCount} samples`)
-          return
-      }
-      // Sample arrived within acceptable, expected time range.
-      clbk(sample)
-      lastSample = { sample, time: tNow, i: nextSampleIndex }
-    }
-  });
+        // Setup infinite streaming from static data set.
+        let tStart = window.performance.now()
+        let pushedDataCount = 0
+        const streamData = () => {
+            const tNow = window.performance.now()
+            // NOTE: This code is for example purposes (streaming stable data rate without destroying browser when switching tabs etc.)
+            // In real use cases, data should be pushed in when it comes.
+            const shouldBeDataPointsCount = Math.floor((sampleRateHz * (tNow - tStart)) / 1000)
+            const newDataPointsCount = Math.min(shouldBeDataPointsCount - pushedDataCount, 100) // Add max 100 samples per frame into a series. This prevents massive performance spikes when switching tabs for long times
+            if (newDataPointsCount > 0) {
+                channelList.forEach((channel, i) => {
+                    const newDataPoints = []
+                    for (let iDp = 0; iDp < newDataPointsCount; iDp++) {
+                        const iData = (pushedDataCount + iDp) % channel.data.length
+                        const sample = channel.data[iData]
+                        newDataPoints.push(sample)
+                    }
+                    channel.heatmapSeries2D.addIntensityValues(newDataPoints)
+                    channel.surfaceSeries3D.addValues({ yValues: newDataPoints })
+                })
+                pushedDataCount += newDataPointsCount
+            }
+            requestAnimationFrame(streamData)
+        }
+        streamData()
+    })
